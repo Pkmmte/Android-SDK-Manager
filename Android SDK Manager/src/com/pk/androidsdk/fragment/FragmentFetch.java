e9a06938f1c373e7f8c00c3bf634e6464116ded9
Pkmmte <pkx7225@gmail.com>
2013-10-31 15:03:39 -0700

    asdfsdfsadfsadfsa

--- Android SDK Manager/src/com/pk/androidsdk/fragment/FragmentFetch.java
+++ Android SDK Manager/src/com/pk/androidsdk/fragment/FragmentFetch.java
@@ -1,20 +1,31 @@
 package com.pk.androidsdk.fragment;
 
-import com.pk.androidsdk.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.pk.androidsdk.R;
 
 public class FragmentFetch extends Fragment
 {
+	// Views
+	private Button btnFetch;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.fragment_fetch, container, false);
 		
+		initViews(view);
+		
 		return view;
+	}
+	
+	private void initViews(View v)
+	{
+		btnFetch = (Button) v.findViewById(R.id.btnFetch);
 	}
 }
